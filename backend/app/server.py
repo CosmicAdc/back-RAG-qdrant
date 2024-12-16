b8e69ebb73bd325d84da4f6641c585760d9d639c
kelly <kelly.paltin23@gmail.com>
2024-12-16 11:07:06 -0500

    Revert "conexion con el frontend"
    This reverts commit d765fb014a76c1141bd33ded50dd6312fc5b004a.

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,35 +1,31 @@
 import asyncio
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from langserve import add_routes
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import routes_qdrant, routes_upload  
-from app.bdd import qdrant_manage, information_manage
+
+from app.constants import constants
+from app.bdd import qdrant_manage,information_manage
 from app.AI.retriever import get_retriever_with_keywords
-from app.routes import routes_history,routes_upload,routes_qdrant,routes_bd
+from app.routes import routes_history,routes_upload,routes_qdrant
 
 
 app = FastAPI()
 
-# Permitir acceso CORS desde el frontend
-origins = [
-    "http://localhost:3000", 
-]
 
 app.include_router(routes_upload.router)
 app.include_router(routes_qdrant.router)
-app.include_router(routes_bd.router)
+
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-app.include_router(routes_upload.router)
-app.include_router(routes_qdrant.router)
 
 @app.get("/")
 async def redirect_root_to_docs():
@@ -38,4 +34,4 @@ async def redirect_root_to_docs():
 if __name__ == "__main__":
     import uvicorn
     asyncio.run(information_manage.execute()) 
-    uvicorn.run(app, host="0.0.0.0", port=8100, loop="asyncio")
+    uvicorn.run(app, host="0.0.0.0", port=8100,loop="asyncio")
